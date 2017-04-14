7f82b86cba062d190e6f9d06e081730e5afc01ac
Wladimir Wendland <wladimir.wendland@gmail.com>
2017-04-14 19:31:32 +0200

    Add Articles and Polymorphic Comments

---
+++ app/Comment.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    public function commentable()
+    {
+    	return $this->morphTo();
+    }
+
+    public function replies()
+    {
+    	return $this->hasMany(Comment::class, 'parent_id');
+    }
+
+    public function parent()
+    {
+    	return $this->belongsTo(Comment::class, 'parent_id');
+    }
+
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+    }
+}
